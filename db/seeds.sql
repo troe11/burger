9c770960834473f997882f09d3bfc0aeeef78d61
Travis Roemhild <travis.roemhild@gmail.com>
2018-02-19 16:26:32 -0700

    updated schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,2 @@
 insert into burgers(burger_name) 
-	values ('Bacon Cheddar'),('Jalepeno Chicken Ranch'),('Dry Tofu Burger');
+	values ('Bacon Cheddar',0),('Jalepeno Chicken Ranch',0),('Dry Tofu Burger',0);
